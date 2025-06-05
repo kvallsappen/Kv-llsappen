b11ac3d006c1b1940e1550363292cca2e489169d
kvallsappen <kvallsappen@gmail.com>
2025-06-05 09:30:10 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -3,9 +3,10 @@ const firebaseConfig = {
   apiKey: "AIzaSyCrRNcYcUgac1_77qpX5yChMFzpbUzf4hA",
   authDomain: "kvallsappen-edb32.firebaseapp.com",
   projectId: "kvallsappen-edb32",
-  storageBucket: "kvallsappen-edb32.appspot.com",
+  storageBucket: "kvallsappen-edb32.firebasestorage.app",
   messagingSenderId: "876798721742",
-  appId: "1:876798721742:web:cd93518f5035302899da9f"
+  appId: "1:876798721742:web:9b20525df5c356ec99da9f"
 };
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
